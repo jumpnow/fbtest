85fe6fe91f63d5fdc4673246bd0a862f6c6db1b4
Besto Apache <bestoapache@gmail.com>
2019-01-31 16:49:20 +0800

    Change border option to 'B', add buffer index support.

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 /*
   Simple framebuffer testing program
- 
+
   Set the screen to a given color. For use in developing Linux
   display drivers.
-  
-  Copyright (c) 2014, Jumpnow Technologies, LLC 
+
+  Copyright (c) 2014, Jumpnow Technologies, LLC
   All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
-  modification, are permitted provided that the following conditions 
+  modification, are permitted provided that the following conditions
   are met:
 
-  1. Redistributions of source code must retain the above copyright 
+  1. Redistributions of source code must retain the above copyright
      notice, this list of conditions and the following disclaimer.
 
   2. Redistributions in binary form must reproduce the above copyright
@@ -19,7 +19,7 @@
      documentation and/or other materials provided with the distribution.
 
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
   HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
@@ -27,7 +27,7 @@
   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
   Ideas taken from the Yocto Project psplash program.
@@ -43,7 +43,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <sys/ioctl.h> 
+#include <sys/ioctl.h>
 #include <sys/mman.h>
 #include <linux/fb.h>
 #include <getopt.h>
@@ -53,6 +53,7 @@ struct fb_config
 	int fd;
 	int width;
 	int height;
+	int height_virtual;
 	int bpp;
 	int stride;
 	int red_offset;
@@ -61,6 +62,7 @@ struct fb_config
 	int green_length;
 	int blue_offset;
 	int blue_length;
+	int buffer_num;
 	char *data;
 	char *base;
 };
@@ -70,24 +72,26 @@ void dump_vscreeninfo(struct fb_var_screeninfo *fvsi)
 	printf("======= FB VAR SCREENINFO =======\n");
 	printf("xres: %d\n", fvsi->xres);
 	printf("yres: %d\n", fvsi->yres);
+	printf("yres_virtual: %d\n", fvsi->yres_virtual);
+	printf("buffer number: %d\n", fvsi->yres_virtual / fvsi->yres);
 	printf("bpp : %d\n", fvsi->bits_per_pixel);
 	printf("red bits    :\n");
 	printf("    offset   : %d\n", fvsi->red.offset);
 	printf("    length   : %d\n", fvsi->red.length);
-	printf("    msb_right: %d\n", fvsi->red.msb_right);	
+	printf("    msb_right: %d\n", fvsi->red.msb_right);
 	printf("green bits  :\n");
 	printf("    offset   : %d\n", fvsi->green.offset);
 	printf("    length   : %d\n", fvsi->green.length);
-	printf("    msb_right: %d\n", fvsi->green.msb_right);	
+	printf("    msb_right: %d\n", fvsi->green.msb_right);
 	printf("blue bits   :\n");
 	printf("    offset   : %d\n", fvsi->blue.offset);
 	printf("    length   : %d\n", fvsi->blue.length);
-	printf("    msb_right: %d\n", fvsi->blue.msb_right);	
+	printf("    msb_right: %d\n", fvsi->blue.msb_right);
 	printf("transp bits :\n");
 	printf("    offset   : %d\n", fvsi->transp.offset);
 	printf("    length   : %d\n", fvsi->transp.length);
-	printf("    msb_right: %d\n", fvsi->transp.msb_right);	
-	
+	printf("    msb_right: %d\n", fvsi->transp.msb_right);
+
 	printf("=================================\n");
 }
 
@@ -103,11 +107,13 @@ void dump_fscreeninfo(struct fb_fix_screeninfo *ffsi)
 
 void plot_pixel(struct fb_config *fb, int x, int y, int r, int g, int b)
 {
-	int offset = (y * fb->stride) + (x * (fb->bpp >> 3)); 
-
+	int offset = (y * fb->stride) + (x * (fb->bpp >> 3));
+	/* FIXME assume the format is BGRA */
 	*(fb->data + offset) = b;
 	*(fb->data + offset + 1) = g;
 	*(fb->data + offset + 2) = r;
+	/* Set aplha to 255 */
+	*(fb->data + offset + 3) = 255;
 }
 
 void draw_rect(struct fb_config *fb, int x, int y, int w, int h, int r, int g, int b)
@@ -118,7 +124,7 @@ void draw_rect(struct fb_config *fb, int x, int y, int w, int h, int r, int g, i
 		for (dx = 0; dx < w; dx++) {
 			plot_pixel(fb, x + dx, y + dy, r, g, b);
 		}
-	}  
+	}
 }
 
 void clear_screen(struct fb_config *fb, int r, int g, int b)
@@ -128,10 +134,11 @@ void clear_screen(struct fb_config *fb, int r, int g, int b)
 
 void usage(const char *argv_0)
 {
-	printf("\nUsage %s: [-r<red>] [-g<green>] [-b<blue>] [-n<border>]\n", argv_0);
-    printf("  All colors default to 0xff\n");
-    printf("  The border color applies to all rgb and is 10 pixels wide\n");
-    printf("  If border is not provided, none is drawn.\n\n");
+	printf("\nUsage %s: [-r<red>] [-g<green>] [-b<blue>] [-B<border>] [-i<buffer index>]\n", argv_0);
+	printf("  All colors default to 0xff\n");
+	printf("  The border color applies to all rgb and is 10 pixels wide\n");
+	printf("  If border is not provided, none is drawn.\n");
+	printf("  The index defaults to 0, if your FB has more than 1 buffers, you can set it.\n");
 	exit(1);
 }
 
@@ -147,8 +154,9 @@ int main(int argc, char **argv)
 	int blue = 0xff;
 	int border = -1;
 	int opt;
-		
-	while ((opt = getopt(argc, argv, "r:g:b:n:h")) != -1) {
+	int index = 0;
+
+	while ((opt = getopt(argc, argv, "r:g:b:B:i:h")) != -1) {
 		switch (opt) {
 		case 'r':
 			red = 0xff & strtol(optarg, NULL, 0);
@@ -162,8 +170,12 @@ int main(int argc, char **argv)
 			blue = 0xff & strtol(optarg, NULL, 0);
 			break;
 
-		case 'n':
+		case 'B':
 			border = 0xff & strtol(optarg, NULL, 0);
+			break;
+
+		case 'i':
+			index = strtol(optarg, NULL, 0);
 			break;
 
 		default:
@@ -180,12 +192,12 @@ int main(int argc, char **argv)
 	}
 
 	if (ioctl(fd, FBIOGET_VSCREENINFO, &fvsi) < 0) {
-		perror("ioctl(FBIOGET_VSCREENINFO)");	
+		perror("ioctl(FBIOGET_VSCREENINFO)");
 		close(fd);
 		exit(1);
 	}
 
-	// dump_vscreeninfo(&fvsi);
+	dump_vscreeninfo(&fvsi);
 
 	if (ioctl(fd, FBIOGET_FSCREENINFO, &ffsi) < 0) {
 		perror("ioctl(FBIOGET_FSCREENINFO)");
@@ -193,11 +205,12 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	// dump_fscreeninfo(&ffsi);
+	dump_fscreeninfo(&ffsi);
 
 	fb.fd = fd;
 	fb.width = fvsi.xres;
 	fb.height = fvsi.yres;
+	fb.height_virtual = fvsi.yres_virtual;
 	fb.bpp = fvsi.bits_per_pixel;
 	fb.stride = ffsi.line_length;
 	fb.red_offset = fvsi.red.offset;
@@ -206,9 +219,14 @@ int main(int argc, char **argv)
 	fb.green_length = fvsi.green.length;
 	fb.blue_offset = fvsi.blue.offset;
 	fb.blue_length = fvsi.blue.length;
+	fb.buffer_num = fb.height_virtual / fb.height;
 
-	fb.base = (char *)mmap((caddr_t) NULL, fb.stride * fb.height,
-			PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (index > fb.buffer_num - 1 || index < 0) {
+		printf("Invalid index.\n");
+		exit(1);
+	}
+
+	fb.base = (char *)mmap((caddr_t) NULL, ffsi.smem_len, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
 	if (fb.base == (char *)-1) {
 		perror("mmap");
@@ -218,17 +236,19 @@ int main(int argc, char **argv)
 
 	offset = (unsigned long) ffsi.smem_start % (unsigned long) getpagesize();
 
-	fb.data = fb.base + offset;
-
+	fb.data = fb.base + offset + (fb.stride * fb.height) * index;
 	if (border == -1) {
 		clear_screen(&fb, red, green, blue);
 	}
 	else {
 		clear_screen(&fb, border, border, border);
 
-		// draw a rect 10 pixels in from each border in the color chosen	
+		// draw a rect 10 pixels in from each border in the color chosen
 		draw_rect(&fb, 10, 10, fb.width - 20, fb.height - 20, red, green, blue);
 	}
+	fvsi.yoffset = index * fvsi.yres;
+	printf("data addr = %p, yoffset = %d\n", fb.data, fvsi.yoffset);
+	ioctl(fb.fd, FBIOPAN_DISPLAY, &fvsi);
 
 	close(fd);
 
